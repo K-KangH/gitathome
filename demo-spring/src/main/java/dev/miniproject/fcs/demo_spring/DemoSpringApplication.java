0c4d941c0683d0e6b45c95040f726e10a350b05d
K-KangH <daus1006@gmail.com>
2024-07-06 23:45:59 +0900

    start

---
+++ demo-spring/src/main/java/dev/miniproject/fcs/demo_spring/DemoSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.miniproject.fcs.demo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringApplication.class, args);
+	}
+
+}
---
+++ demo-spring/src/test/java/dev/miniproject/fcs/demo_spring/DemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.miniproject.fcs.demo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
